0da550d761f383ebf71ba2945b3ceb45c1ca5cd2
Caio Domingos <caiod.dev@gmail.com>
2023-04-02 17:44:57 -0300

    correcoes Caio 2

--- Fase 6/exercicios.js
+++ Fase 6/exercicios.js
@@ -1,99 +1,100 @@
 // 1. Crie um objeto que represente uma pessoa, com as propriedades nome, idade, e-mail e telefone.
 const pessoa1 = {
-  nome: "Matheus", 
-  sobrenome: "Lucio",
+  nome: 'Matheus',
+  sobrenome: 'Lucio',
   idade: 24,
-  email: "matheuslucio@yahoo.com",
-  telefone: "(31) 97521-5821"
-}
+  email: 'matheuslucio@yahoo.com',
+  telefone: '(31) 97521-5821',
+};
 
-console.log(pessoa1)
+console.log(pessoa1);
 
 // 2. Crie um objeto que represente um carro, com as propriedades marca, modelo, ano e cor.
 
 const carro1 = {
-  marca: "Fiat",
-  modelo: "Toro",
+  marca: 'Fiat',
+  modelo: 'Toro',
   ano: 2023,
-  cor: "Prata"
-}
+  cor: 'Prata',
+};
 
-console.log(carro1)
+console.log(carro1);
 
 // 3. Crie um objeto que represente um livro, com as propriedades título, autor, número de páginas e preço.
 
 const livro1 = {
-  titulo: "Senhor dos Anéis",
-  autor: "Tolkien",
+  titulo: 'Senhor dos Anéis',
+  autor: 'Tolkien',
   paginas: 50,
-  preço: 75
-}
+  preço: 75,
+};
 
-console.log(livro1)
+console.log(livro1);
 
 // 4. Crie uma função que receba um objeto pessoa como argumento e retorne o nome completo da pessoa (concatenando o nome e o sobrenome, se houver).
 
-function nomePessoa(pessoa){
-  return pessoa.nome + " " + pessoa.sobrenome
+function nomePessoa(pessoa) {
+  return pessoa.nome + ' ' + pessoa.sobrenome;
 }
 
-console.log(nomePessoa(pessoa1))
+console.log(nomePessoa(pessoa1));
 
 // 5. Crie uma função que receba um objeto carro como argumento e retorne uma string com a marca e o modelo do carro.
 
-function modeloCarro(carro){
-  return carro.marca + ", " + carro.modelo
+function modeloCarro(carro) {
+  return carro.marca + ', ' + carro.modelo;
 }
 
-console.log(modeloCarro(carro1))
+console.log(modeloCarro(carro1));
 
 // 6. Crie uma função que receba um objeto livro como argumento e retorne uma string com o título e o autor do livro.
 
-function tituloLivro(livro){
-  return livro.titulo + ", " + livro.autor
+function tituloLivro(livro) {
+  return livro.titulo + ', ' + livro.autor;
 }
 
-console.log(tituloLivro(livro1))
+console.log(tituloLivro(livro1));
 
 // 7. Crie uma função que receba um objeto pessoa como argumento e retorne uma string com uma mensagem de saudação personalizada (por exemplo, "Olá, João! Tudo bem?").
 
-function saudacaoPessoa(pessoa){
-  return 'Seja bem vindo ' + pessoa.nome + " " + pessoa.sobrenome + '!'
+function saudacaoPessoa(pessoa) {
+  // return 'Seja bem vindo ' + pessoa.nome + " " + pessoa.sobrenome + '!'
+  return `Seja bem vindo ${pessoa.nome} ${pessoa.sobrenome}!`;
 }
 
-console.log(saudacaoPessoa(pessoa1))
+console.log(saudacaoPessoa(pessoa1));
 
 // 8. Crie uma função que receba um objeto carro como argumento e retorne o ano do carro como número.
 
-function anoCarro(carro){
+function anoCarro(carro) {
   // const anoFormatado = parseInt(carro.ano)
-  return parseInt(carro.ano)
+  return parseInt(carro.ano);
 }
 
-console.log(anoCarro(carro1))
+console.log(anoCarro(carro1));
 
 // 9. Crie uma função que receba um objeto livro como argumento e retorne o preço do livro como número.
 
-function precoLivro(livro){ 
-return parseInt(livro.preço)
+function precoLivro(livro) {
+  return parseInt(livro.preço);
 }
 
-console.log(precoLivro(livro1))
+console.log(precoLivro(livro1));
 
 // 10. Crie um objeto que represente uma playlist de músicas, com as propriedades nome, autor e músicas (um array de objetos com as propriedades nome e duração). Crie uma função que receba esse objeto como argumento e retorne o tempo total de duração das músicas da playlist.
 
 const Playlist = {
-  nome: "Album",
-  autor: "Jorge e Matheus",
+  nome: 'Album',
+  autor: 'Jorge e Matheus',
   musicas: [
-    { nome: "musica1", duracao: 3 },
-    { nome: "musica2", duracao: 4 },
-    { nome: "musica3", duracao: 2 },
+    { nome: 'musica1', duracao: 3 },
+    { nome: 'musica2', duracao: 4 },
+    { nome: 'musica3', duracao: 2 },
   ],
-}
+};
 
-function duracaoMusicas(playlist){
+function duracaoMusicas(playlist) {
   return playlist.musicas.reduce((acc, musica) => acc + musica.duracao, 0);
 }
 
-console.log(duracaoMusicas(Playlist))
+console.log(duracaoMusicas(Playlist));
--- Fase 7/loja.js
+++ Fase 7/loja.js
@@ -1,57 +1,96 @@
 // Crie um objeto que represente um produto, com as propriedades nome, preço e quantidade em estoque.
 
-const cadeiraGamer = {
-    nome: "Cadeira Gamer LRS Gamer X PRO",
-    preco: 1250,
-    estoque: 42
-}
+const produto1 = {
+  nome: 'Cadeira Gamer LRS Gamer X PRO',
+  preco: 1250,
+  estoque: 42,
+};
+const produto2 = {
+  nome: 'Mouse Gamer LRS Gamer X PRO',
+  preco: 200,
+  estoque: 55,
+};
 
 // Crie um objeto que represente um pedido, com as propriedades id, data, produtos (um array de objetos) e total.
 
 const pedido1 = {
-    identificador: 001,
-    data: "01/04/2023",
-    produtos: [cadeiraGamer, cadeiraGamer],
-    total: 2500
-}
+  identificador: 001,
+  data: '01/04/2023',
+  produtos: [produto1, produto1],
+};
+
+const pedido2 = {
+  identificador: 001,
+  data: '01/04/2023',
+  produtos: [
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto1,
+    produto2,
+  ],
+};
 
 // Crie uma função que receba um objeto produto como argumento e retorne o preço total (preço x quantidade em estoque).
 
-function verificacaoEstoquePreco(cadeiraGamer){
-    const precoTotal = cadeiraGamer.estoque * cadeiraGamer.preco
-    return parseInt(precoTotal)  
+function verificacaoEstoquePreco(produto) {
+  const precoTotal = produto.estoque * produto.preco;
+  return parseInt(precoTotal);
 }
 
-console.log(verificacaoEstoquePreco(cadeiraGamer))
+// console.log('------------------');
 
+// console.log(verificacaoEstoquePreco(produto1));
+// console.log(verificacaoEstoquePreco(produto2));
+
+// return;
 // Crie uma função que receba um objeto pedido como argumento e retorne o total do pedido (soma dos preços totais de todos os produtos do pedido).
 
-function totalPedido() {
-   return pedido1.total
+function totalPedido(pedido) {
+  // encontrar os produtos dentro do pedido
+  const produtos = pedido.produtos;
+
+  // encontrar o preço de cada produto
+  const valoresProdutos = produtos.map((produto) => {
+    return produto.preco;
+  });
+
+  // somar os valores dos produtos
+  const total = valoresProdutos.reduce((acumulador, valorAtual) => {
+    return acumulador + valorAtual;
+  }, 0);
+
+  // retornar o total
+  return total;
 }
-  
-  console.log(totalPedido(pedido1));
-  
+function totalPedido2(pedido) {
+  return pedido.produtos
+    .map((produto) => produto.preco)
+    .reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
+}
+
+console.log(totalPedido(pedido1));
+console.log(totalPedido2(pedido1));
+console.log(totalPedido(pedido2));
+
+return;
 
 // Crie uma função que receba um objeto pedido como argumento e retorne uma string com a lista de produtos do pedido, cada um em uma linha, no formato "nome do produto - quantidade x preço unitário = preço total".
 
-function stringPedido(){
- 
-  }
-  
+function stringPedido() {}
 
-console.log(stringPedido(pedido1))
+console.log(stringPedido(pedido1));
 // Crie uma função que receba um objeto pedido e um objeto produto como argumentos e adicione o produto ao array de produtos do pedido.
-
-
 
 // Crie uma função que receba um objeto pedido e um objeto produto como argumentos e remova o produto do array de produtos do pedido.
 
-
-
 // Crie uma função que receba um objeto pedido e um número como argumentos e retorne um array com os nomes dos produtos que o cliente comprou mais do que o número fornecido.
 
-
-
 // Crie uma função que receba um objeto pedido e um número como argumentos e retorne um array com os nomes dos produtos que o cliente comprou menos do que o número fornecido.
-
