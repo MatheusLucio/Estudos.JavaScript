036b54f4d8fd14306efbe713f1921294857dd38a
Matheus Lucio <matheusluciotst@yahoo.com>
2023-04-04 15:24:41 -0300

    att

--- Fase 7/loja-2.js
+++ Fase 7/loja-2.js
@@ -1,10 +1,55 @@
 // Crie um objeto que represente um cliente, com as propriedades nome, e-mail, telefone e um array de objetos que representam os pedidos do cliente.
+
+const produto1 = {
+    valor: 200
+}
+
+
+const cliente1 = {
+    nome: "João Gabriel",
+    email: "joaogabrielsft@gmail.com",
+    telefone: "38 3332-4550",
+    pedido: [produto1, produto2, produto1, produto3]
+}
+
 // Crie uma função que receba um objeto cliente e retorne o total de gastos do cliente na loja (soma dos totais de todos os pedidos do cliente).
+
+
+
+
 // Crie uma função que receba um objeto cliente e um objeto de pedido como argumentos e adicione o pedido ao array de pedidos do cliente.
+
+
+
+
 // Crie uma função que receba um objeto cliente e um objeto de pedido como argumentos e remova o pedido do array de pedidos do cliente.
+
+
+
+
 // Crie uma função que receba um objeto cliente e um número como argumentos e retorne um array com os nomes dos produtos que o cliente comprou mais do que o número fornecido.
+
+
+
+
 // Crie uma função que receba um objeto cliente e um número como argumentos e retorne um array com os nomes dos produtos que o cliente comprou menos do que o número fornecido.
+
+
+
+
 // Crie uma função que receba um objeto cliente e retorne uma string com uma lista HTML dos pedidos do cliente, cada um em um elemento <li> com o nome do produto, a quantidade e o preço total do pedido.
+
+
+
+
 // Crie uma função que receba um objeto cliente e retorne uma string com uma lista HTML dos pedidos do cliente agrupados por data, cada um em um elemento <li> com o nome do produto, a quantidade e o preço total do pedido.
+
+
+
+
 // Crie uma função que receba um objeto cliente e retorne uma string com uma lista HTML dos pedidos do cliente agrupados por produto, cada um em um elemento <li> com o nome do produto, a quantidade e o preço total do pedido.
+
+
+
+
 // Crie uma função que receba um objeto cliente e retorne uma string com uma lista HTML dos pedidos do cliente agrupados por data e produto, cada um em um elemento <li> com o nome do produto, a quantidade e o preço total do pedido.
