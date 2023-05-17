0930c43a73957fd11869bf63c9bc74bdd447b2a5
Matheus Lucio <matheusluciotst@yahoo.com>
2023-05-17 20:16:07 -0300

    att

---
+++ Fase 8/Sistema-Desafiador-Biblioteca.js
@@ -0,0 +1,28 @@
+// Retorne um novo array contendo apenas os livros emprestados.
+// Encontre o cliente com o maior número de empréstimos realizados.
+// Encontre o livro mais emprestado.
+// Retorne um novo array contendo apenas os clientes que realizaram empréstimos no mês de maio.
+// Difícil
+// Encontre todos os livros emprestados por um cliente específico e retorne uma lista com os títulos dos livros.
+// Encontre todos os empréstimos realizados em um determinado livro e retorne uma lista com os nomes dos clientes que emprestaram o livro.
+// Encontre todos os empréstimos que estão atrasados e retorne uma lista com os nomes dos clientes, títulos dos livros e data de devolução esperada.
+// Crie uma função que retorne a média de empréstimos realizados por cliente.
+// Encontre todos os livros emprestados mais de uma vez e retorne uma lista com os títulos dos livros.
+// Muito Difícil
+// Crie uma função que retorne uma lista com os livros que nunca foram emprestados.
+// Crie uma função que retorne uma lista com os clientes que realizaram empréstimos de livros de um determinado autor.
+// Crie uma função que retorne o cliente com o maior atraso em devoluções de empréstimos.
+// Crie uma função que retorne o livro com a menor média de dias de empréstimo.
+// Crie uma função que retorne a data em que um livro foi mais emprestado.
+// Desafiador
+// Crie uma função que retorne a lista de livros mais emprestados em ordem crescente.
+// Crie uma função que retorne a lista de clientes que realizaram mais empréstimos em ordem crescente.
+// Crie uma função que retorne a lista de empréstimos em ordem decrescente pela data de empréstimo.
+// Crie uma função que retorne a lista de livros que foram emprestados por mais de um cliente em ordem alfabética.
+// Crie uma função que retorne a lista de clientes que realizaram empréstimos de todos os livros.
+// Extremamente Desafiador
+// Crie uma função que retorne a lista de clientes que realizaram empréstimos em todos os meses do ano.
+// Crie uma função que retorne a lista de livros que nunca foram emprestados em ordem alfabética.
+// Crie uma função que retorne a lista de empréstimos que foram realizados com atraso em ordem decrescente pela quantidade de dias de atraso.
+// Crie uma função que retorne a lista de clientes que realizaram empréstimos em todos os meses do ano em ordem alfabética.
+// Crie uma função que retorne a lista de livros mais emprestados por clientes com menos de 30 anos em ordem decrescente pela quantidade de empréstimos.
